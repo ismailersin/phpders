ea426ca818409dd1dc370529d3bed6cca7b1f869
“oyunbeyim” <oyunbeyim@gmail.com>
2024-12-17 12:30:06 +0300

    ex eklendi

---
+++ ex3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<table border="1">
+<?php
+$sayac=1;
+while($sayac<100)
+{
+
+echo "<tr>
+<td>1</td>
+<td>2</td>
+<td>3</td>
+<td>4</td>
+<td>5</td>
+</tr>
+";
+
+
+    $sayac++;
+}
+?>
+
+
+</table>
+
+
+    
+</body>
+</html>
