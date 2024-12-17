02f0246ab8e122b5bf531ef9d5178236d342b082
“oyunbeyim” <oyunbeyim@gmail.com>
2024-12-17 12:06:42 +0300

    ex2 eklendi

---
+++ ex2.php
@@ -0,0 +1,7 @@
+<?php
+$kullanici_adi="gazimtal";
+$sifre="123456";
+if ($kullanici_adi=="gazimtal" and $sifre=="123456")
+echo "<strong><em> Giriş Başarılı Bir Şekilde Yapıldı </strong></em>";
+
+?>
